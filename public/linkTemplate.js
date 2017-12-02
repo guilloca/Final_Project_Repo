b5eb2303bdf6d9b8f29aa4e243d7c1f6aeac7931
AlexGraalum <alexgraalum@yahoo.com>
2017-12-01 21:54:47 -0800

    Committed

--- public/linkTemplate.js
+++ public/linkTemplate.js
@@ -1,12 +1,15 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['linkTemplate'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+templates['linkTemplate'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data,blockParams,depths) {
+    var helper, alias1=container.escapeExpression, alias2=depth0 != null ? depth0 : (container.nullContext || {}), alias3=helpers.helperMissing, alias4="function";
 
-  return "<div class=\"gameLink\">\r\n  <a href=\"/\">\r\n    <img class=\"linkPhoto\" src="
-    + alias4(((helper = (helper = helpers.photoURL || (depth0 != null ? depth0.photoURL : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"photoURL","hash":{},"data":data}) : helper)))
+  return "<div class=\"gameLink\">\r\n  <a href=\"/playlist/"
+    + alias1(container.lambda((depths[1] != null ? depths[1].permalink : depths[1]), depth0))
+    + alias1(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"index","hash":{},"data":data}) : helper)))
+    + "\">\r\n    <img class=\"linkPhoto\" src="
+    + alias1(((helper = (helper = helpers.photoURL || (depth0 != null ? depth0.photoURL : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"photoURL","hash":{},"data":data}) : helper)))
     + ">\r\n    <h6 class=\"linkTitle\">"
-    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
+    + alias1(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h6>\r\n    </img>\r\n  </a>\r\n</div>\r\n";
-},"useData":true});
+},"useData":true,"useDepths":true});
 })();
