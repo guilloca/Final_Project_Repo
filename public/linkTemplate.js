6ae7ef97eda8847a1a9cdd4dd4e115907d5187af
Jonathan Hull <hulljo@oregonstate.edu>
2017-12-01 21:24:55 -0800

    my Butt

--- public/linkTemplate.js
+++ public/linkTemplate.js
@@ -3,10 +3,10 @@
 templates['linkTemplate'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div class=\"gameLink\">\r\n  <a href=\"/\">\r\n    <img class=\"linkPhoto\" src="
+  return "<div class=\"gameLink\">\n  <a href=\"/\">\n    <img class=\"linkPhoto\" src="
     + alias4(((helper = (helper = helpers.photoURL || (depth0 != null ? depth0.photoURL : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"photoURL","hash":{},"data":data}) : helper)))
-    + ">\r\n    <h6 class=\"linkTitle\">"
+    + ">\n    <h6 class=\"linkTitle\">"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</h6>\r\n  </a>\r\n</div>\r\n";
+    + "</h6>\n  </a>\n</div>\n";
 },"useData":true});
 })();
