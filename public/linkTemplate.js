9cd26114f91c9f4e6d3ed1e9eb5b434272083e58
Jonathan Hull <hulljo@oregonstate.edu>
2017-12-06 11:25:57 -0800

    styled back pause/play and skip buttons

--- public/linkTemplate.js
+++ public/linkTemplate.js
@@ -3,13 +3,13 @@
 templates['linkTemplate'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data,blockParams,depths) {
     var helper, alias1=container.escapeExpression, alias2=depth0 != null ? depth0 : (container.nullContext || {}), alias3=helpers.helperMissing, alias4="function";
 
-  return "<div class=\"gameLink\">\r\n  <a href=\"/games/"
+  return "<div class=\"gameLink\">\n  <a href=\"/games/"
     + alias1(container.lambda((depths[1] != null ? depths[1].permalink : depths[1]), depth0))
     + alias1(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"index","hash":{},"data":data}) : helper)))
-    + "\">\r\n    <img class=\"linkPhoto\" src="
+    + "\">\n    <img class=\"linkPhoto\" src="
     + alias1(((helper = (helper = helpers.photoURL || (depth0 != null ? depth0.photoURL : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"photoURL","hash":{},"data":data}) : helper)))
-    + ">\r\n    <h6 class=\"linkTitle\">"
+    + ">\n    <h6 class=\"linkTitle\">"
     + alias1(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</h6>\r\n    </img>\r\n  </a>\r\n</div>\r\n";
+    + "</h6>\n    </img>\n  </a>\n</div>\n";
 },"useData":true,"useDepths":true});
 })();
