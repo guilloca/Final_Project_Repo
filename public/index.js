1f3d104eae4ffe84cbdea33b6e40b95ba2b74018
guilloca <32156785+guilloca@users.noreply.github.com>
2017-12-06 15:23:41 -0800

    removed .html's and removed unused code

--- public/index.js
+++ public/index.js
@@ -86,40 +86,6 @@ function onPlayerReady(event) {
     });
 }
 
-/* **** Shiz that won't work for no good reason ****
-var listID = document.getElementById('player-content').playlistid;
-if (!listID) {
-    console.log("Error: ListID is empty");
-}
-console.log("JS Loaded w/ ListID: ") + String(listID);
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '300',
-        width: '300',
-        playerVars: {
-            listType: 'playlist',
-            list: listID
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-
-    });
-}
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        done = true;
-    }
-}
-function stopVideo() {
-    player.stopVideo();
-}
-*/
 function insertNewLink(title, photoURL) {
   var context = {
     title: title,
